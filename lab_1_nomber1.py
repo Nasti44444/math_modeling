216844efcf3f06ce57f99c916f0d7d2f9e80cdfd
Nasti44444 <146091753+Nasti44444@users.noreply.github.com>
2023-10-02 20:46:01 +0000

    Lab_1

---
+++ lab_1_nomber1.py
@@ -0,0 +1,8 @@
+print ("Helloy, World!")
+
+print (" Маша + Петя = Любовь")
+
+print (" 3 + 4 ")
+
+print ( 3 + 4 )
+
---
+++ lab_1_nomber2.py
@@ -0,0 +1,8 @@
+print (type (" Hellow, World! "))
+
+print (type ( 3 + 4 ))
+
+print (type (3 / 4))
+
+a = [ 1, 2, 5, 10, 100 ]
+print (type (a))
